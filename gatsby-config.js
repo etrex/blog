b49f2f28fbdaefd80e4731623f6c5cb1618b0f51
etrex kuo <et284vu065k3@gmail.com>
2020-06-28 03:22:34 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,17 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `卡米哥的記憶體`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,43 @@
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+    </Layout>
+  );
 }
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
